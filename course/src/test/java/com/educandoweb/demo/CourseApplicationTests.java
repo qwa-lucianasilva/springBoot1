b42bec8431cf8de267f85ef27a7701fd2a5ba632
Luciana Silva <luciana.silva@qwasolucoes.com.br>
2022-05-30 11:05:28 -0300

    Primeiro commit

---
+++ course/src/test/java/com/educandoweb/demo/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
